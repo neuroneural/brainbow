ffe95cbd75e920408ac2ed49ff016c3968a8dd19
Pavel Popov <paavali.popov@gmail.com>
2023-10-12 01:31:18 -0400

    extracted nipy functions without depricated stuff

--- process_image.py
+++ process_image.py
@@ -4,30 +4,18 @@ import json
 import numpy as np
 import nibabel as nib
 
-from scipy.ndimage import affine_transform, shift
-from nibabel.processing import resample_from_to, resample_to_output
-from nibabel.affines import apply_affine, to_matvec, from_matvec
-from scipy.ndimage import zoom
-
-
-# from nibabel.viewers import OrthoSlicer3D
-
 from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-# from nipy import load_image
-# from nipy.core.api import xyz_affine
-# from nipy.labs.viz import plot_map, coord_transform
+# from nipy.labs.viz import plot_map
 
-
-# from pylab import savefig, subplot, subplots_adjust, figure, rc, text
-
+from nibabel.affines import apply_affine
 
 from src.colormaps import CMAPS, DSGN
 from src.utils import process_output
-from src.slicer import OrthoSlicer3D
+from src.nipy import plot_map
 
 
 def process_image(
@@ -48,83 +36,13 @@ def process_image(
     nifti = nib.load(NIFTI)
     nifti_affine = nifti.affine
     nifti_data = nifti.get_fdata()
-    # for idx in range(nifti_data.shape[-1]):
 
     anat = nib.load(ANAT)
     anat_affine = anat.affine
     anat_data = anat.get_fdata()
 
-    # new_nifti_data = []
-    # new_nifti_affines = []
-
-    # for idx in range(5):
-    #     single_component = nifti_data[:, :, :, idx]
-    #     single_component = nib.Nifti1Image(
-    #         single_component, nifti.affine, header=nifti.header
-    #     )
-    #     single_component = resample_from_to(single_component, anat)
-
-    #     new_nifti_data += [single_component.get_fdata()]
-    #     new_nifti_affines += [single_component.affine]
-
-    # # # upscale nifti images
-    # reference = nib.Nifti1Image(
-    #     nifti_data[:, :, :, 0], nifti.affine, header=nifti.header
-    # )
-    # anat = resample_from_to(anat, reference)
-    # anat_data = anat.get_fdata()
-    # anat_affine = anat.affine
-
-    # nifti_voxels = np.diag(to_matvec(nifti.affine)[0])
-    # anat_voxels = np.diag(to_matvec(anat.affine)[0])
-    # ratio = nifti_voxels / anat_voxels
-    # nifti_affine = from_matvec(np.eye(3), to_matvec(nifti.affine)[1])
-    # # for idx in range(nifti_data.shape[-1]):
-    # for idx in range(5):
-    #     single_component = nifti_data[:, :, :, idx]
-    #     # print(single_component.shape)
-    #     upscaled_data = zoom(single_component, zoom=ratio, order=3)
-    #     # print(upscaled_data.shape)
-
-    #     new_nifti_data += [upscaled_data]
-    # nifti_data = np.stack(new_nifti_data, axis=-1)
-    # print(nifti_data.shape)
-
-    # Apply affine transform to data
-    # mode = "wrap"
-    # for idx in range(nifti_data.shape[-1]):
-    #     component = nifti_data[:, :, :, idx]
-    #     nifti_data[:, :, :, idx] = shift(
-    #         input=affine_transform(
-    #             input=component,
-    #             matrix=np.linalg.inv(nifti_affine),
-    #             mode=mode
-    #         ),
-    #         shift=np.array(component.shape)/2,
-    #         mode=mode,
-    #     )
-    # anat_data = shift(
-    #     input=affine_transform(
-    #         input=anat_data,
-    #         matrix=np.linalg.inv(anat_affine),
-    #         mode=mode
-    #     ),
-    #     shift=np.array(anat_data.shape)/2,
-    #     mode=mode,
-    # )
-
-    # inv_affine = transform.AffineTransform(np.linalg.inv(anat_affine), dimensionality=3)
-    # affine = transform.AffineTransform(anat_affine,  dimensionality=3)
-    # anat_data1 = transform.warp(anat_data, inverse_map=inv_affine)
-    # anat_data2 = transform.warp(anat_data, inverse_map=affine)
-
     print(f"Anat shape: {anat_data.shape}")
-
-    # # ### Apply affine transform to anat_data
-    # reference = nib.Nifti1Image(nifti_data[:, :, :, 0], nifti.affine, header=nifti.header)
-    # anat = resample_from_to(anat, reference)
-    # anat_data = anat.get_fdata()
-    # # whole_aff = np.linalg.inv(img2.affine).dot(img1.affine.dot(slice_aff))
+    print(f"Nifti shape: {nifti_data.shape}")
 
     # derive a few things for plotting
     vmax = nifti_data.max()
@@ -132,8 +50,7 @@ def process_image(
     mcmap = CMAPS[SGN]
     imshow_args = {"vmax": vmax, "vmin": vmin, "cmap": mcmap}
 
-    # n_features = nifti.shape[-1]
-    n_features = 5
+    n_features = nifti.shape[-1]
     n_cols = max([1, round(np.sqrt(n_features / 3))])
     # 3 here is the number of views; for the figure layout it accounts that
     # the column's width is greater than the row's height
@@ -141,44 +58,16 @@ def process_image(
 
     print("Plotting components:")
 
-    # create figure and subplots
-    # fig, ax = plt.subplots(
-    #     ncols=n_cols,
-    #     nrows=n_rows,
-    #     figsize=(iscale * n_cols, iscale * n_rows / 3),
-    #     facecolor = 'black',
-    # )
-
-    # fig = plt.figure(figsize=(iscale * n_cols, iscale * n_rows / 3), facecolor = 'black')
-    # DEBUGGING
-    fig = plt.figure(figsize=(iscale * n_cols, iscale * n_rows))
+    fig = plt.figure(figsize=(iscale * n_cols, iscale * n_rows / 3), facecolor="black")
     gs = gridspec.GridSpec(ncols=n_cols, nrows=n_rows)
     fig.subplots_adjust(
         left=0.01, right=0.99, bottom=0.01, top=0.99, wspace=0.1, hspace=0
     )
 
     # plot components
-    # for i in tqdm(range(n_features)):
-    for i in tqdm(range(5)):
+    for i in tqdm(range(n_features)):
         # set subplots
-        # inner_gs = gridspec.GridSpecFromSubplotSpec(
-        #     ncols=3,
-        #     nrows=1,
-        #     subplot_spec=gs[i],
-        #     width_ratios=[1.2, 1, 1],
-        # )
-        # ax = [plt.subplot(inner_gs[j]) for j in range(9)]
-        # DEBUGGING
-        inner_gs = gridspec.GridSpecFromSubplotSpec(
-            ncols=3,
-            nrows=3,
-            subplot_spec=gs[i],
-            # width_ratios=[1.2, 1, 1],
-            # height_ratios=[1, 1, 1]
-        )
-        ax0 = [plt.subplot(inner_gs[0, j]) for j in range(3)]
-        ax1 = [plt.subplot(inner_gs[1, j]) for j in range(3)]
-        ax2 = [plt.subplot(inner_gs[2, j]) for j in range(3)]
+        ax = plt.subplot(gs[i])
 
         # load the ICA component and filter it according to SGN
         data = nifti_data[:, :, :, i]
@@ -186,107 +75,23 @@ def process_image(
         # plot component
         if max(abs(data.flatten())) > thr + 0.2:
             max_idx = np.unravel_index(np.argmax(data), data.shape)
-            # max_coordinate = apply_affine(nifti_affine, max_idx)
-            max_coordinate = apply_affine(nifti_affine, max_idx)
-            # print(f"cut indices: {max_idx}")
-            # print(f"cut coordinates: {max_coordinate}")
+            cut_coords = apply_affine(nifti_affine, max_idx)
 
-            masked_data = np.ma.masked_inside(
-                data, v1=-thr - 0.2, v2=thr + 0.2, copy=False
-            )
-            # overlay_data = [
-            #     masked_data[max_idx[0], :, :].T,
-            #     masked_data[:, max_idx[1], :].T,
-            #     masked_data[:, :, max_idx[2]].T,
-            # ]
-
-            # underlay_data = [
-            #     anat_data[max_idx[0], :, :].T,
-            #     anat_data[:, max_idx[1], :].T,
-            #     anat_data[:, :, max_idx[2]].T,
-            # ]
-
-            OrthoSlicer3D(
-                anat_data,
-                anat_affine,
-                axes=ax0,
-                position=max_coordinate,
-                underlay=True,
-                imshow_args=imshow_args,
-            )
-            OrthoSlicer3D(
-                masked_data,
+            slicer = plot_map(
+                data,
                 nifti_affine,
-                axes=ax0,
-                position=max_coordinate,
-                underlay=False,
-                imshow_args=imshow_args,
+                anat=anat_data,
+                anat_affine=anat_affine,
+                black_bg=True,
+                threshold=thr,
+                cut_coords=cut_coords,
+                annotate=False,
+                axes=ax,
+                draw_cross=False,
+                **imshow_args,
             )
 
-            OrthoSlicer3D(
-                anat_data,
-                anat_affine,
-                axes=ax1,
-                position=max_coordinate,
-                underlay=True,
-                imshow_args=imshow_args,
-            )
-            OrthoSlicer3D(
-                masked_data,
-                nifti_affine,
-                axes=ax2,
-                position=max_coordinate,
-                underlay=False,
-                imshow_args=imshow_args,
-            )
-            # underlay_slicer.show()
-            # for plane in range(3):
-
-            # ax[plane].imshow(underlay_data[plane], cmap='Greys', origin='upper', extent=[0, 1, 0, 1])
-            # ax[plane].imshow(overlay_data[plane], cmap=mcmap, alpha=0.5, origin='upper', extent=[0, 1, 0, 1])
-            # ax[plane].axis('off')
-            # ax[plane].grid(False)
-            # DEBUGGING
-
-            # slicer = OrthoSlicer3D()
-
-            # ax0[plane].imshow(underlay_data[plane], cmap='gray', origin='lower', extent=[0, 1, 0, 1])
-            # ax0[plane].imshow(overlay_data[plane], cmap=mcmap, alpha=0.5, origin='lower', extent=[0, 1, 0, 1], **imshow_args)
-            # ax0[plane].axis('off')
-            # ax0[plane].grid(False)
-
-            # ax1[plane].imshow(underlay_data[plane], cmap='gray', origin='lower', extent=[0, 1, 0, 1])
-            # ax1[plane].axis('off')
-            # ax1[plane].grid(False)
-
-            # ax2[plane].imshow(overlay_data[plane], cmap=mcmap, alpha=0.5, origin='lower', extent=[0, 1, 0, 1], **imshow_args)
-            # ax2[plane].axis('off')
-            # ax2[plane].grid(False)
-
-            # plot_map(
-            #     data,
-            #     xyz_affine(nifti),
-            #     anat=anat.get_data(),
-            #     anat_affine=xyz_affine(anat),
-            #     black_bg=True,
-            #     threshold=thr,
-            #     cut_coords=coord_transform(
-            #         max_idx[0], max_idx[1], max_idx[2], xyz_affine(nifti)
-            #     ),
-            #     annotate=False,
-            #     axes=ax[i],
-            #     cmap=mcmap,
-            #     draw_cross=False,
-            #     **imshow_args,
-            # )
-            # text(
-            #     0.0,
-            #     0.95,
-            #     str(i),
-            #     transform=ax.transAxes,
-            #     horizontalalignment="center",
-            #     color=(1, 1, 1),
-            # )
+            slicer.annotate(size=8)
 
     # save results
     if save_dir is None:
--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,10 @@
+""" A few helper functions, many of which come from nipy"""
+
 import os
-
+import warnings
 import numpy as np
-import nibabel as nib
-import matplotlib.pyplot as plt
+from scipy import ndimage
 
-def load_image(filename):
-    return nib.load(filename)
-
-# def xyz_affine(voxel_size, origin):
-#     """Create an affine transformation matrix from voxel size and origin."""
-#     return np.diag([*voxel_size, 1]) * np.eye(4, dtype=np.float32) @ np.array([origin[0], origin[1], origin[2], 1])
-
-# def plot_map(ax, affine, title=None):
-#     """Plot a map of a NIfTI image.
-
-#     Args:
-#         image: A NIfTI image.
-#         affine: The affine transformation matrix of the image.
-#         title: The title of the plot.
-#     """
-
-#     fig, ax = plt.subplots(1, 1)
-#     ax.imshow(image.get_data(), cmap='gray', origin='lower')
-#     ax.set_title(title)
-#     ax.set_aspect('equal')
-#     ax.axis('off')
-#     plt.show()
 
 def process_output(output: str, save_dir: str = None):
     # create output directory
@@ -50,3 +29,77 @@ def process_output(output: str, save_dir: str = None):
             ext = ["svg"]
 
     return output, ext
+
+
+def is_numlike(obj):
+    """Return True if `obj` looks like a number"""
+    try:
+        obj + 1
+    except:
+        return False
+    return True
+
+
+def is_iterable(obj):
+    """Return True if `obj` is iterable"""
+    try:
+        iter(obj)
+    except TypeError:
+        return False
+    return True
+
+
+def get_bounds(shape, affine):
+    """Return the world-space bounds occupied by an array given an affine."""
+    adim, bdim, cdim = shape
+    adim -= 1
+    bdim -= 1
+    cdim -= 1
+    # form a collection of vectors for each 8 corners of the box
+    box = np.array(
+        [
+            [0.0, 0, 0, 1],
+            [adim, 0, 0, 1],
+            [0, bdim, 0, 1],
+            [0, 0, cdim, 1],
+            [adim, bdim, 0, 1],
+            [adim, 0, cdim, 1],
+            [0, bdim, cdim, 1],
+            [adim, bdim, cdim, 1],
+        ]
+    ).T
+    box = np.dot(affine, box)[:3]
+    return list(zip(box.min(axis=-1), box.max(axis=-1)))
+
+
+def get_mask_bounds(mask, affine):
+    """Return the world-space bounds occupied by a mask given an affine.
+
+    Notes
+    -----
+
+    The mask should have only one connect component.
+
+    The affine should be diagonal or diagonal-permuted.
+    """
+    (xmin, xmax), (ymin, ymax), (zmin, zmax) = get_bounds(mask.shape, affine)
+    slices = ndimage.find_objects(mask)
+    if len(slices) == 0:
+        warnings.warn("empty mask", stacklevel=2)
+    else:
+        x_slice, y_slice, z_slice = slices[0]
+        x_width, y_width, z_width = mask.shape
+        xmin, xmax = (
+            xmin + x_slice.start * (xmax - xmin) / x_width,
+            xmin + x_slice.stop * (xmax - xmin) / x_width,
+        )
+        ymin, ymax = (
+            ymin + y_slice.start * (ymax - ymin) / y_width,
+            ymin + y_slice.stop * (ymax - ymin) / y_width,
+        )
+        zmin, zmax = (
+            zmin + z_slice.start * (zmax - zmin) / z_width,
+            zmin + z_slice.stop * (zmax - zmin) / z_width,
+        )
+
+    return xmin, xmax, ymin, ymax, zmin, zmax
